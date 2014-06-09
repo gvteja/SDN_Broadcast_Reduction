830d54c1299fb4b4090af069696c4eeb06b7763f
Vijay Teja <gvteja@gmail.com>
2014-06-09 01:10:47 -0700

    Cleaned up and fixed bug

--- Pox_Controller/ext/global_switch_stats.py
+++ Pox_Controller/ext/global_switch_stats.py
@@ -1,25 +1,7 @@
-'''
-/*******************************************************************************
- *   Copyright 2014 Karishma Sureka , Sai Gopal , Vijay Teja
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *******************************************************************************/
-'''
-
 import pox.openflow.libopenflow_01 as of
 from pox.core import core
-import pox
-import time
+from pox.lib.recoco import Timer
+
 log = core.getLogger("Global_Stats")
 
 rx_packets = 0
@@ -28,48 +10,75 @@ rx_dropped = 0
 tx_dropped = 0
 rx_errors = 0
 tx_errors = 0
-n = 0
-cn = 0
+num_sw = 0 #total num of switches
+num_done = 0 #num of switches for whom stats have been collected
+wait_timeout = 0
+handler = {} #{dpid:portstats listner handler}
 
-#maybe reg and unreg listener in global stats fn so that we wont count the normal switch stats. better would be to have a bool map of all the switches that have recvd stats
+
+
+def _init():
+    global num_sw
+    global num_done
+    global handler
+    global rx_packets
+    global tx_packets
+    global rx_dropped
+    global tx_dropped
+    global rx_errors
+    global tx_errors
+    handler = {}
+    num_done = 0
+    num_sw = len(core.openflow.connections)
+    rx_packets = 0
+    tx_packets = 0
+    rx_dropped = 0
+    tx_dropped = 0
+    rx_errors = 0
+    tx_errors = 0
+
 
 def global_stats():
-	global n
-	global cn
-	cn = 0
-	n = core.openflow.connections.keys().__len__()
-	log.info("sending port stats req to all {0} switches".format(n))
-	for con in core.openflow.connections:
-		con.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
-	time.sleep(1)
-	while(cn != n):
-		time.sleep(1)
-	log.info('global stats :\nrx_packets : {0} tx_packets : {1} rx_dropped : {2} tx_dropped : {3} rx_errors : {4} tx_errors : {5}'.format(rx_packets, tx_packets ,rx_dropped ,tx_dropped ,rx_errors ,tx_errors))
+    _init()
+    log.info("Sending port stats req to all {0} switches".format(num_sw))
+    for con in core.openflow.connections:
+        handler[con.dpid] = con.addListenerByName("PortStatsReceived", _stats_listener)
+        con.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+    Timer(wait_timeout, _check)
 
-def stats_listener(event):
-	#log.info(type(event))
-	global cn
-	global rx_packets
-	global tx_packets
-	global rx_dropped
-	global tx_dropped
-	global rx_errors
-	global tx_errors
-	cn = cn + 1
-	log.info("switch " + str(event.connection.dpid) + " port statistics")
-	for port_stats in event.stats:
-		if(port_stats.port_no != of.OFPP_LOCAL):#no stats for openflow local port.
-			rx_packets += port_stats.rx_packets
-			tx_packets += port_stats.tx_packets
-			rx_dropped += port_stats.rx_dropped
-			tx_dropped += port_stats.tx_dropped
-			rx_errors += port_stats.rx_errors
-			tx_errors += port_stats.tx_errors
+def _check():
+    if num_done != num_sw: #means some stats not recvd for some switches. print anyway
+        print_stats()
 
-			log.info('port : {0} rx_packets : {1} tx_packets : {2} rx_dropped : {3} tx_dropped : {4} rx_errors : {5} tx_errors : {6}'.format(port_stats.port_no, port_stats.rx_packets, port_stats.tx_packets ,port_stats.rx_dropped ,port_stats.tx_dropped ,port_stats.rx_errors ,port_stats.tx_errors))
-	
+def _stats_listener(event):
+    event.connection.removeListener(handler[event.dpid])
+    global num_done
+    global rx_packets
+    global tx_packets
+    global rx_dropped
+    global tx_dropped
+    global rx_errors
+    global tx_errors
+    num_done += 1
+    log.debug('Received port statistics for switch {0}'.format(event.dpid))
+    for port_stats in event.stats:
+        if(port_stats.port_no != of.OFPP_LOCAL):#no stats for openflow local port.
+            rx_packets += port_stats.rx_packets
+            tx_packets += port_stats.tx_packets
+            rx_dropped += port_stats.rx_dropped
+            tx_dropped += port_stats.tx_dropped
+            rx_errors += port_stats.rx_errors
+            tx_errors += port_stats.tx_errors
+
+        #log.debug('port : {0} rx_packets : {1} tx_packets : {2} rx_dropped : {3} tx_dropped : {4} rx_errors : {5} tx_errors : {6}'.format(port_stats.port_no, port_stats.rx_packets, port_stats.tx_packets ,port_stats.rx_dropped ,port_stats.tx_dropped ,port_stats.rx_errors ,port_stats.tx_errors))
+    if num_done == num_sw: #check if all switches are done
+        print_stats()
+
+def print_stats():
+    log.info('Aggregated switch stats :\nrx_packets : {0} tx_packets : {1} rx_dropped : {2} tx_dropped : {3} rx_errors : {4} tx_errors : {5}'.format(rx_packets, tx_packets ,rx_dropped ,tx_dropped ,rx_errors ,tx_errors))
 
 
-def launch():
-	core.Interactive.variables['gstats'] = global_stats
-	core.openflow.addListenerByName("PortStatsReceived", stats_listener)
+def launch(wait_time = 30):
+    global wait_timeout
+    wait_timeout = int(wait_time)
+    core.Interactive.variables['gstats'] = global_stats
